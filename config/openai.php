ee8eeb91ba5ddbe88e4135ae2e968185f846db15
Sprinkle7 <allauddin2015@gmail.com>
2024-12-17 16:46:58 +0500

    Chat GPT Push

--- config/openai.php
+++ config/openai.php
@@ -8,7 +8,6 @@ return [
     | The OpenAI API key to use for requests. You can set this in your .env file.
     |
     */
-    'api_key' => env('OPENAI_API_KEY', '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'),
 
     /*
     |--------------------------------------------------------------------------
