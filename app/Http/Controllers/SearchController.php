79cf6bb75afe4f3967030aa7bcf18441a7c78a2c
Sprinkle7 <allauddin2015@gmail.com>
2025-01-24 19:54:20 +0500

    Email Template

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -46,7 +46,7 @@ class SearchController extends Controller
 
     public function loadMore(Request $request)
     {
-        $perPage = 12; 
+        $perPage = $request->get('size'); 
         $results = Company::select(['firmen_id','anrede', 'webseite', 'firmenname', 'vorname', 'nachname', 'id', 'jobtitel', 'hausnummer', 'strasse', 'ort', 'plz', 'land','telefonnummer','branche_hauptkategorie']) 
                    ->paginate($perPage);
 
