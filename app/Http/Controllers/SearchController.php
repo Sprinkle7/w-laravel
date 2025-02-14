8fd0a613e70c505abc20a74eb1062d9bcf4324be
Sprinkle7 <allauddin2015@gmail.com>
2025-02-14 21:42:38 +0500

    Email Template

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -30,11 +30,13 @@ class SearchController extends Controller
     public function details($id)
     {
         $cacheKey = 'details_' . $id;
+        
         $details = Cache::remember($cacheKey, 60, function () use ($id) {
             $explode = explode('_', $id);
+            $firmenname = str_replace('_', ' ', $explode[2]);
             return Company::where('vorname', '=', $explode[0])
                             ->where('nachname', '=', $explode[1])
-                            ->where('firmenname', '=', $explode[2])
+                            ->where('firmenname', '=', $firmenname)
                             ->first() ?? [];
         });
 
