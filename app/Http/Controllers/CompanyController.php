f48c764cf062bf725864b8b2bd568d119e11b2d7
Sprinkle7 <allauddin2015@gmail.com>
2025-02-27 21:38:17 +0500

    Email Template

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -631,7 +631,6 @@ class CompanyController extends Controller
     public function bulkDelete(Request $request)
     {
         $ids = $request->input('ids');
-        dd($ids);
         if (!empty($ids)) {
             Company::whereIn('id', $ids)->delete();
             return response()->json(['message' => 'Ausgewählte Datensätze wurden erfolgreich gelöscht'], 200);
